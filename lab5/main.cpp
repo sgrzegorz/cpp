b2b08b0dc405534d8c154896c2e1c2498aa24b03
gs <grzegorzsroka2@gmail.com>
2018-04-16 20:27:38 +0200

    all

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,72 +1,7 @@
 #include <iostream>
 #include <cstring>
+#include "tree.h"
 using namespace std;
-
-class TreeException;
-
-template<typename T>class NodeBST
-{
-    NodeBST(const NodeBST<T> &){}
-    NodeBST<T>& operator =(NodeBST<T>&&);
-    T data;
-    NodeBST<T>* left;
-    NodeBST<T>* right;
-public:
-    NodeBST(T _data) : data(_data), left(NULL), right(NULL) {}
-    bool contains(T val) const;
-    bool insert(T val);
-    void print() const;
-};
-
-template<typename T> bool NodeBST<T>::contains(T val) const {
-    if (val == data) return true;
-    NodeBST* next = (val < data) ? left : right;
-    return (next != NULL) ? next->contains(val) : false;
-}
-
-template<typename T>bool NodeBST<T>::insert(T val) {
-    if (val == data) return false;
-    NodeBST* & next = (val < data) ? left : right;
-    if (next == NULL) {
-        next = new NodeBST(val);
-        return true;
-    }
-    else return next->insert(val);
-}
-
-template<typename T>void NodeBST<T>::print() const {
-    if (left != NULL) left->print();
-    cout << data << " ";
-    if (right != NULL) right->print();
-}
-
-template<typename T>class TreeBST
-{
-    TreeBST(const TreeBST<T> &){}
-    TreeBST<T>& operator =(TreeBST<T>&&);
-
-    NodeBST<T>* root;
-public:
-    TreeBST() : root(NULL) {}
-    bool contains(T val) const { return (root != NULL) ? root->contains(val) : false; }
-    bool insert(T val);
-    void print() const;
-};
-
-template<typename T>bool TreeBST<T>::insert(T val) {
-    if (root == NULL) {
-        root = new NodeBST<T>(val);
-        return true;
-    }
-    else return root->insert(val);
-}
-
-template<typename T>void TreeBST<T>::print() const {
-    cout << "tree: [";
-    if (root == NULL) cout << "empty" << endl;
-    else root->print();
-    cout << "]" << endl;
-}
 
 
 
@@ -78,15 +13,17 @@ int main() {
     T.print();
 
     TreeBST<string> S;
-    S.insert("aaa");
+    S.insert("1");
     S.insert("ff");
     S.insert("fafa");
-    S.insert(1);
+    S.insert("1");
     S.insert("KKKGKF");
     S.insert("5");
     S.insert("98kkkkkkkk");
     S.insert("98");
     S.print();
+    cout<<endl;
+    cout<<S;
 }
 
 /*
--- lab5/tree.h
+++ lab5/tree.h
@@ -1,12 +1,30 @@
-
-
 #ifndef LAB5_TREE_H
 #define LAB5_TREE_H
+#include <iostream>
+using namespace std;
+class TreeException;
+template<typename T>class NodeBST;
+template <typename T> class TreeBST;
+template <typename T> ostream& operator<< (ostream&, TreeBST<T>&);//!!!!!!!!!!!!!!!!!!!!!!!!
 
-
-class tree {
-
+template<typename T>class TreeBST
+{
+    TreeBST(const TreeBST<T> &){}
+    TreeBST<T>& operator =(TreeBST<T>&&);
+    NodeBST<T>* root;
+public:
+    TreeBST() : root(NULL) {}
+    bool contains(T val) const { return (root != NULL) ? root->contains(val) : false; }
+    bool insert(T val);
+    void print() const;
+    friend ostream& operator<< <>(ostream&, TreeBST<T>&);
 };
+
+template <typename T> ostream& operator<<(ostream& os, TreeBST<T>& tree){
+    if (tree.root == NULL) cout << "empty" << endl;
+    else os<<tree.root;
+    return os;
+}
 
 
 #endif //LAB5_TREE_H
